a90c8a2b1f64e991bc900b3c1af71fd90058d5cf
Raul125 <71040879+Raul125@users.noreply.github.com>
2021-04-15 19:16:36 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("F0CC4E27-CDC5-4217-9EE0-F42147223510")]
+[assembly: Guid("")]
 
 // Version information for an assembly consists of the following four values:
 //
